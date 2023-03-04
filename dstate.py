9c9bc5bc77f84e3297e98b5b749cec6a6c54068c
Alexander Ptakhin <me@aptakhin.name>
2023-03-04 09:20:40 +0100

    Add persister

--- dstate.py
+++ dstate.py
@@ -31,6 +31,12 @@ class DState(object):
 MType = TypeVar('MType')
 
 
+@dataclass
+class DReference:
+    cls: MType
+    ref: dict[str, Any]
+
+
 class DataContainer(object):
     def __init__(self) -> None:
         pass
@@ -44,26 +50,56 @@ class DataContainer(object):
 
 class StatePersister(ABC):
     @abstractmethod
-    def load(self, ref: dict[str, Any], default_state: str) -> StateMachineData:
+    def load(self, default_state: str) -> StateMachineData:
         pass
 
     @abstractmethod
-    def save(self, ref: dict[str, Any], state: StateMachineData) -> None:
+    def save(self, state: StateMachineData) -> None:
         pass
 
 
 class InMemoryPersister(StatePersister):
-    def __init__(self, obj: dict[str, Any]) -> None:
+    def __init__(self, obj: dict[str, Any], key: str) -> None:
         self._obj = obj
+        self.key = key
 
-    def load(self, ref: dict[str, Any], default_state: str) -> StateMachineData:
+    def load(self, default_state: str) -> StateMachineData:
         return StateMachineData(
-            state=self._obj.get(ref['id'], {}).get('state', default_state)
+            state=self._obj.get(self.key, {}).get('state', default_state)
         )
 
-    def save(self, ref: dict[str, Any], state: StateMachineData) -> None:
-        self._obj.setdefault(ref['id'], {})
-        self._obj[ref['id']]['state'] = state.state
+    def save(self, state: StateMachineData) -> None:
+        self._obj.setdefault(self.key, {})
+        self._obj[self.key]['state'] = state.state
+
+
+class PersisterCreator(object):
+    @abstractmethod
+    def get_or_create(self, machine_cls, ref) -> StatePersister:
+        pass
+
+
+class InMemoryPersisterCreators(PersisterCreator):
+    def __init__(self) -> None:
+        self._obj: dict[str, Any] = {}
+
+    def get_or_create(self, machine_cls, ref) -> InMemoryPersister:
+        return InMemoryPersister(self._obj, key=ref['id'])
+
+
+class WorldPersisterCreators(object):
+    def __init__(self) -> None:
+        self._creators: dict[str, PersisterCreator] = {}
+
+    def register(self, name: str, persister_creator: PersisterCreator) -> None:
+        self._creators[name] = persister_creator
+
+    def get_creator(self, name: str) -> PersisterCreator:
+        return self._creators[name]
+
+    def get_or_create_persister(self, name, machine_cls, ref) -> StatePersister:
+        creator = self.get_creator(name)
+        return creator.get_or_create(machine_cls, ref)
 
 
 class Lock(ABC):
@@ -138,8 +174,11 @@ class WorldLockCreators(object):
     def register(self, name: str, lock_creator: LockCreator) -> None:
         self._creators[name] = lock_creator
 
+    def get_creator(self, name: str) -> LockCreator:
+        return self._creators[name]
+
     def get_or_create_lock(self, lock_name, machine_cls, ref) -> Lock:
-        creator = self._creators[lock_name]
+        creator = self.get_creator(lock_name)
         return creator.get_or_create(machine_cls, ref)
 
 
@@ -148,24 +187,19 @@ class World(object):
 
     def __init__(
         self,
-        lock_creator: Optional[WorldLockCreators] = None,
+        persister_creators: Optional[WorldPersisterCreators] = None,
+        lock_creators: Optional[WorldLockCreators] = None,
     ) -> None:
-        self._state_machine_persisters = {}
+        self._persister_creators: WorldPersisterCreators = (
+            persister_creators or WorldPersisterCreators()
+        )
+        self._persister_creators.register('default', InMemoryPersisterCreators())
 
         self._in_memory_obj = {}
 
-        self._lock_creators: WorldLockCreators = lock_creator or WorldLockCreators()
+        self._lock_creators: WorldLockCreators = lock_creators or WorldLockCreators()
         self._lock_creators.register('lock', InMemoryLockCreator())
         self._lock_creators.register('none', NoLockCreator())
-
-        # self._contexts = {}
-        # different locks support pg for row, or redis for row.
-
-    def register_state_machine_persister(self, cls: Any, persister):
-        pass
-
-    def register_(self, cls: Any, persister):
-        pass
 
     @contextmanager
     def lock_and_write_machine(
@@ -174,23 +208,27 @@ class World(object):
         ref: dict[str, Any],
         initial: Optional[str] = None,
         lock_name: str = 'lock',
-        lock_creator: Optional[WorldLockCreators] = None,
+        lock_creators: Optional[WorldLockCreators] = None,
+        persister_name: str = 'default',
+        persister_creators: Optional[WorldPersisterCreators] = None,
     ) -> ContextManager[MType]:
-        lock_creators = lock_creator or self._lock_creators
+        lock_creators = lock_creators or self._lock_creators
         lock = lock_creators.get_or_create_lock(lock_name, machine_cls, ref)
         lock.lock(timeout=timedelta(seconds=3), lock_time=timedelta(seconds=3))
 
-        persister = self._get_persister(machine_cls, ref)
-        state_data = persister.load(ref, initial)
+        persister_creators = persister_creators or self._persister_creators
+        persister_creator = persister_creators.get_creator(persister_name)
+        persister = persister_creator.get_or_create(machine_cls, ref)
+        state_data = persister.load(initial)
 
-        def notify(dstate, ref, persister):
+        def notify(dstate, persister):
             state_data = dstate.make_state_data()
-            print('Want save', ref, state_data.__dict__)
-            persister.save(ref, state_data)
+            print('Want save', state_data.__dict__)
+            persister.save(state_data)
 
         state = DState(
             state=state_data.state,
-            save_here=partial(notify, ref=ref, persister=persister),
+            save_here=partial(notify, persister=persister),
         )
         machine = machine_cls(state)
         try:
