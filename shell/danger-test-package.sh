c53e5843cbf022a6eb95c1895cc03f83145961aa
Alexander Ptakhin <me@aptakhin.name>
2023-03-05 12:07:09 +0100

    Added linux sed ersion

--- shell/danger-test-package.sh
+++ shell/danger-test-package.sh
@@ -13,8 +13,13 @@ cp -rf ./ ../dstate-test-package
 # Cwd
 cd ../dstate-test-package
 
-# Rewrite names to install dstate: Mac sed version
-sed -i '' 's/name = "dstate"/name = "dstate-test-package"/' pyproject.toml
+# Rewrite names to install dstate
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' 's/name = "dstate"/name = "dstate-test-package"/' pyproject.toml
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sed -i 's/name = "dstate"/name = "dstate-test-package"/' pyproject.toml
+fi
+# Thx: https://stackoverflow.com/a/17072017
 
 # Install dstate from the latest build library
 poetry add dist/`ls dist/ | sort -r | head -n 1`
